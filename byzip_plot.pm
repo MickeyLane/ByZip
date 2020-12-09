23bb10d346e04c0b7b4e7e8a4e62620b0f683ed7
MickeyLane <34776617+MickeyLane@users.noreply.github.com>
2020-12-09 06:47:04 -0500

    Plot update

--- byzip_plot.pm
+++ byzip_plot.pm
@@ -7,8 +7,10 @@ use strict;
 # This file is part of byzip.pl. See information at the top of that file
 #
 
+use GD::Graph qw (black white green orange blue red);
 use GD::Graph::points;
 use GD::Graph::lines;
+# use GD::Graph::colour;
 use List::Util qw(min);
 
 #
@@ -16,8 +18,8 @@ use List::Util qw(min);
 #
 #   $csv:
 #
-#      1st row: Date,Cured,Sick,UntestedSick,Dead,[repeat Cured..Dead],[repeat]
-#      2nd row: YYYY-MM-DD, N, N, N (etc.)
+#      1st row: Date,New,Cured,Sick,UntestedSick,Dead,[repeat Cured..Dead],[repeat]
+#      2nd row: YYYY-MM-DD, M, N, N, N, N (etc.)
 #
 #   $sims:
 #
@@ -32,9 +34,12 @@ sub make_plot {
 
     my @csv_array = @$csv_ptr;
     
+    my $debug_prints = 0;
     my $number_of_columns_per_sim = 4;
     my $graph_file = "$dir/graph.gif";
-    print ("\$graph_file = $graph_file\n");
+    if ($debug_prints) {
+        print ("\$graph_file = $graph_file\n");
+    }
 
     #
     # Remove the title row and count columns
@@ -44,39 +49,51 @@ sub make_plot {
     my @columns = split (',', $column_titles);
     my $column_count = @columns;
 
-    my $computed_number_of_sims = ($column_count - 1) / $number_of_columns_per_sim;
+    my $computed_number_of_sims = ($column_count - 2) / $number_of_columns_per_sim;
 
     my @data;
+
+    # my $row_count_after_removing_date = @csv_array;
+    # print ("\$row_count_after_removing_date = $row_count_after_removing_date\n");
 
     #
     # Make an array containing the 1st value in each row
     #
-    my $data_length_1 = @csv_array;
-    print ("\$data_length_1 = $data_length_1\n");
     my ($array_of_dates_ptr, $dates_to_skip, $new_csv_ptr) = make_array_of_dates (\@csv_array, $begin_display_dt);
-    push (@data, \@$array_of_dates_ptr);
+    push (@data, $array_of_dates_ptr);
     @csv_array = @$new_csv_ptr;
-    
-    #
-    # Debug
-    #
-    my $top_row = $csv_array[0];
-    print ("\$top_row = $top_row\n");
+    if ($debug_prints) {
+        my $row_count_after_removing_date = @csv_array;
+        print ("\$row_count_after_removing_date = $row_count_after_removing_date\n");
+        my $top_row = $csv_array[0];
+        my $row_length_after_removing_date = length ($top_row);
+        print ("\$row_length_after_removing_date = $row_length_after_removing_date\n");
+    }
 
-    my $data_length_2 = @$array_of_dates_ptr;
-    print ("\$data_length_2 = $data_length_2\n");
+    #
+    # Make an array of new case counts
+    #
+    my ($array_of_new_cases_ptr, $new_csv_ptr_2) = make_array_of_new_cases (\@csv_array, $dates_to_skip);
+    push (@data, $array_of_new_cases_ptr);
+    @csv_array = @$new_csv_ptr_2;
+    if ($debug_prints) {
+        my $row_count_after_removing_case_count = @csv_array;
+        print ("\$row_count_after_removing_case_count = $row_count_after_removing_case_count\n");
+        my $top_row = $csv_array[0];
+        my $row_length_after_removing_case_count = length ($top_row);
+        print ("\$row_length_after_removing_case_count = $row_length_after_removing_case_count\n");
+    }
 
     for (my $i = 0; $i < $computed_number_of_sims; $i++) {
-        my ($cured_p, $sick_p, $untested_p, $dead_p, $new_csv_ptr) = get_sim_results (
+        my ($cured_p, $sick_p, $untested_p, $dead_p, $new_csv_ptr_3) = get_sim_results (
             \@csv_array, $number_of_columns_per_sim, $dates_to_skip, $max_cured);
-
-        #
-        # Debug...
-        #
-        my $data_length_3 = @$cured_p;
-        if ($data_length_2 != $data_length_3) {
-            print ("\$data_length_3 = $data_length_3\n");
-            die;
+        @csv_array = @$new_csv_ptr_3;
+        if ($debug_prints) {
+            my $row_count_after_removing_a_sim = @csv_array;
+            print ("\$row_count_after_removing_a_sim = $row_count_after_removing_a_sim\n");
+            my $top_row = $csv_array[0];
+            my $row_length_after_removing_a_sim = length ($top_row);
+            print ("\$row_length_after_removing_a_sim = $row_length_after_removing_a_sim\n");
         }
 
         push (@data, $cured_p);
@@ -85,13 +102,19 @@ sub make_plot {
         push (@data, $dead_p);
     }
 
+    # my @color_array = qw (black);
+    # my @additional_color_array = qw (green orange blue red);
+    # for (my $f = 0; $f <  $computed_number_of_sims; $f++) {
+    #     push (@color_array, @additional_color_array);
+    # }
+
     my $graph = GD::Graph::lines->new (1900, 750);
-    $graph->set( 
+    $graph->set ( 
             x_label 	=> 'Dates', 
             y_label 	=> 'Cases', 
             title  		=> $title, 
             #cumulate 	=> 1, 
-            dclrs 		=> [ 'green', 'orange', 'blue', 'red' ], 
+            # dclrs 		=> @color_array, 
             borderclrs 	=> [ qw(black black), qw(black black) ], 
             bar_spacing => 4, 
             transparent => 0,
@@ -100,6 +123,22 @@ sub make_plot {
             show_values => 0,
             x_labels_vertical => 1
     ) or die $graph->error; 
+
+    if ($computed_number_of_sims == 1) {
+        $graph->set ( 
+            dclrs 		=> [qw(black green orange blue red)]
+        );
+    }
+    elsif ($computed_number_of_sims == 3) {
+        $graph->set ( 
+            dclrs 		=> [qw(black green orange blue red green orange blue red green orange blue red)]
+        );
+    }
+    elsif ($computed_number_of_sims == 5) {
+        $graph->set ( 
+            dclrs 		=> [qw(black green orange blue red green orange blue red green orange blue red green orange blue red green orange blue red)]
+        );
+    }
 
     #
     # @data is an array of arrays
@@ -154,6 +193,34 @@ sub make_array_of_dates {
     }
 
     return (\@return_array, $dates_to_skip, \@new_csv);
+}
+
+sub make_array_of_new_cases {
+    my $ptr = shift;
+    my $dates_to_skip = shift;
+
+    my @csv = @$ptr;
+    my $row_count = @csv;
+    my @new_csv;
+    my @return_array;
+
+    for (my $i = 0; $i < $row_count; $i++) {
+        my $row = shift (@csv);
+        my $i = index ($row, ',');
+        my $count = substr ($row, 0, $i);
+        my $remainder = substr ($row, $i + 1);
+
+        if ($dates_to_skip > 0) {
+            $dates_to_skip--;
+        }
+        else {
+            push (@return_array, $count);
+        }
+
+        push (@new_csv, $remainder);
+    }
+
+    return (\@return_array, \@new_csv);
 }
 
 sub get_sim_results {
